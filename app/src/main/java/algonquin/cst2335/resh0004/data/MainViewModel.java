a257eb608661514a298c9868b5885497874ffde0
Fayz Reshid <resh0004@algonquinlive.com>
2023-05-26 20:42:38 -0400

    Completed Lab 2

--- app/src/main/java/algonquin/cst2335/resh0004/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/resh0004/data/MainViewModel.java
@@ -4,5 +4,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    public MutableLiveData<String> editString= new MutableLiveData<>();
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+    public MutableLiveData<Boolean>  selectButtons = new MutableLiveData<>();
 }
--- app/src/main/java/algonquin/cst2335/resh0004/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/resh0004/ui/MainActivity.java
@@ -1,14 +1,12 @@
 package algonquin.cst2335.resh0004.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import algonquin.cst2335.resh0004.data.MainViewModel;
 import algonquin.cst2335.resh0004.databinding.ActivityMainBinding;
@@ -37,9 +35,48 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        model.selectButtons.observe(this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.switchButton.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+
+            String message = "The value is now: " + selected;
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+        });
+
+        variableBinding.checkBox.setOnCheckedChangeListener((button, isChecked) -> {
+           model.selectButtons.postValue(isChecked);
+        });
+
+        variableBinding.switchButton.setOnCheckedChangeListener((button, isChecked) -> {
+            model.selectButtons.postValue(isChecked);
+        });
+
+        variableBinding.radioButton.setOnCheckedChangeListener((button, isChecked) -> {
+            model.selectButtons.postValue(isChecked);
+        });
+
+        // Initialize the ImageView using ViewBinding
+        ImageView myImageView = variableBinding.imageView;
+        ImageButton myImageButton = variableBinding.imageButton;
+// Set an OnClickListener for the ImageButton
+
+       myImageButton.setOnClickListener(v -> {
+                int width = myImageView.getWidth();
+                int height = myImageView.getHeight();
+
+           String message = "The width = " + width + " and height = " + height;
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        });
+
     }
 
     }
+
+
+
+
 
 
 
