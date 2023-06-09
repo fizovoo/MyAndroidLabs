d6bf9a1303a38d69ef364cefcbfb57976089f30b
Fayz Reshid <resh0004@algonquinlive.com>
2023-06-09 16:33:32 -0400

    Not working version of Lab 4

--- app/src/main/java/algonquin/cst2335/resh0004/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/resh0004/MainActivity.java
@@ -3,11 +3,45 @@ package algonquin.cst2335.resh0004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    @Override
+    protected void onStop() {
+        Log.w(TAG, "The application is no longer visible.");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.w( TAG, "The application no longer responds to user input" );
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.w(TAG,"The application is now responding to user input" );
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.w( TAG, "The application is now visible on screen." );
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.w( TAG,"Any memory used by the application is freed.");
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
