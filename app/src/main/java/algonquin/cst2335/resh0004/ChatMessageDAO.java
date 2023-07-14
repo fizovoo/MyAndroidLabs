fc6265ce8a246e8365a112a15de7adf6f6296d44
Fayz Reshid <resh0004@algonquinlive.com>
2023-07-14 17:11:03 -0400

    Semi Done with Lab 7

---
+++ app/src/main/java/algonquin/cst2335/resh0004/ChatMessageDAO.java
@@ -0,0 +1,18 @@
+package algonquin.cst2335.resh0004;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ChatMessageDAO {
+    @Insert
+    public long insertMessage(ChatMessage m);
+    @Query("Select * FROM ChatMessage")
+    public List<ChatMessage> getAllMessages();
+    @Delete
+    void deleteMessage(ChatMessage m);
+}
